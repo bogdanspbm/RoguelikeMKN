72b60937fe5cb9a2332f42f87a52617918d5c168
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-11-02 16:56:25 +0300

    Key Listener

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
 import engine.processer.GameProcesser;
 
 public class Main {
     public static void main(String[] args) {
+        initGlobalScreen();
         GameProcesser game = new GameProcesser();
         game.start();
+    }
+
+    private static void initGlobalScreen() {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
     }
 }
--- src/main/java/player/controller/PlayerController.java
+++ src/main/java/player/controller/PlayerController.java
@@ -1,7 +1,22 @@
 package player.controller;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import objects.controller.Controller;
+
 import objects.pawn.Pawn;
 
-public class PlayerController extends Controller {
+public class PlayerController extends Controller implements NativeKeyListener {
+
+
+    public PlayerController() {
+        // TODO: Если игра переносится в мультиплеер, то прослушку нужно делать где-то снаружи
+        GlobalScreen.addNativeKeyListener(this);
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nativeEvent) {
+        System.out.println(nativeEvent);
+    }
 }
