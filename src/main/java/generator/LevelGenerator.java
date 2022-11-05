33f6ef9f4537d001c19e57f0223e7c1a9c906104
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-11-05 19:30:56 +0300

    Fixes Generator

--- src/main/java/generator/LevelGenerator.java
+++ src/main/java/generator/LevelGenerator.java
@@ -6,18 +6,8 @@ import java.io.IOException;
 
 public class LevelGenerator {
 
-    static int resolution = 1024;
-
-
 
     public static void main(String[] args) {
-
-        PerlinNoiseGenerator noiseForEachCell = new PerlinNoiseGenerator(0);
-
-        try {
-        ImageIO.write(noiseForEachCell.getMap(),"png", new File("noise.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PerlinNoiseGenerator generator = new PerlinNoiseGenerator(1024);
     }
 }
--- src/main/java/generator/PerlinNoiseGenerator.java
+++ src/main/java/generator/PerlinNoiseGenerator.java
@@ -5,70 +5,66 @@ import java.awt.image.DataBufferInt;
 import java.util.Arrays;
 import java.util.Random;
 
-class PerlinNoiseGenerator
-{
+class PerlinNoiseGenerator {
     private Random random;
-    byte[] permutationTable;
+    private byte[] permutationTable;
 
-    public PerlinNoiseGenerator(int seed)
-    {
-        random = new Random(seed);
-        permutationTable = new byte[1024*1024];
+    private int resolution = 1024;
+
+
+    public PerlinNoiseGenerator(int resolution) {
+        this.resolution = resolution;
+        random = new Random();
+        permutationTable = new byte[resolution * resolution];
         random.nextBytes(permutationTable);
-
     }
 
-    private float[] getPseudoRandomGradientVector(int x, int y)
-    {
-        int v = (int)(((x * 1836311903L) ^ (y * 2971215073L) + 4807526976L) & 1023L);
+    private float[] getPseudoRandomGradientVector(int x, int y) {
+        int v = (int) (((x * 1836311903L) ^ (y * 2971215073L) + 4807526976L) & 1023L);
         v = permutationTable[v] & 3;
-        switch (v)
-        {
-            case 0:  return new float[]{  1, 0 };
-            case 1:  return new float[]{ -1, 0 };
-            case 2:  return new float[]{  0, 1 };
-            default: return new float[]{  0,-1 };
+        switch (v) {
+            case 0:
+                return new float[]{1, 0};
+            case 1:
+                return new float[]{-1, 0};
+            case 2:
+                return new float[]{0, 1};
+            default:
+                return new float[]{0, -1};
         }
     }
 
-    static float qunticCurve(float t)
-    {
+    private float qunticCurve(float t) {
         return t * t * t * (t * (t * 6 - 15) + 10);
     }
 
-    static float lerp(float a, float b, float t)
-    {
+    private float lerp(float a, float b, float t) {
         return a + (b - a) * t;
     }
 
-    static float dot(float[] a, float[] b)
-    {
+    private float dot(float[] a, float[] b) {
         return a[0] * b[0] + a[1] * b[1];
     }
 
-    public float noise(float fx, float fy)
-    {
-        int left = (int)Math.floor(fx);
-        int top  = (int)Math.floor(fy);
+    public float noise(float fx, float fy) {
+        int left = (int) Math.floor(fx);
+        int top = (int) Math.floor(fy);
         float pointInQuadX = fx - left;
         float pointInQuadY = fy - top;
 
-        float[] topLeftGradient     = getPseudoRandomGradientVector(left,   top  );
-        float[] topRightGradient    = getPseudoRandomGradientVector(left+1, top  );
-        float[] bottomLeftGradient  = getPseudoRandomGradientVector(left,   top+1);
-        float[] bottomRightGradient = getPseudoRandomGradientVector(left+1, top+1);
+        float[] topLeftGradient = getPseudoRandomGradientVector(left, top);
+        float[] topRightGradient = getPseudoRandomGradientVector(left + 1, top);
+        float[] bottomLeftGradient = getPseudoRandomGradientVector(left, top + 1);
+        float[] bottomRightGradient = getPseudoRandomGradientVector(left + 1, top + 1);
 
-        float[] distanceToTopLeft     = new float[]{ pointInQuadX,   pointInQuadY   };
-        float[] distanceToTopRight    = new float[]{ pointInQuadX-1, pointInQuadY   };
-        float[] distanceToBottomLeft  = new float[]{ pointInQuadX,   pointInQuadY-1 };
-        float[] distanceToBottomRight = new float[]{ pointInQuadX-1, pointInQuadY-1 };
+        float[] distanceToTopLeft = new float[]{pointInQuadX, pointInQuadY};
+        float[] distanceToTopRight = new float[]{pointInQuadX - 1, pointInQuadY};
+        float[] distanceToBottomLeft = new float[]{pointInQuadX, pointInQuadY - 1};
+        float[] distanceToBottomRight = new float[]{pointInQuadX - 1, pointInQuadY - 1};
 
-//        Vector2D<Float> a = new Vector2D<>(pointInQuadX,pointInQuadY);
-//        Vector2D<Float> b = new Vector2D<>(pointInQuadX-1,pointInQuadY);
-//        a.dot(b);
-        float tx1 = dot(distanceToTopLeft,     topLeftGradient);
-        float tx2 = dot(distanceToTopRight,    topRightGradient);
-        float bx1 = dot(distanceToBottomLeft,  bottomLeftGradient);
+        float tx1 = dot(distanceToTopLeft, topLeftGradient);
+        float tx2 = dot(distanceToTopRight, topRightGradient);
+        float bx1 = dot(distanceToBottomLeft, bottomLeftGradient);
         float bx2 = dot(distanceToBottomRight, bottomRightGradient);
 
         pointInQuadX = qunticCurve(pointInQuadX);
@@ -82,34 +78,44 @@ class PerlinNoiseGenerator
 
     }
 
-    public float noise(float fx, float fy, int octaves, float persistence)
-    {
+    public float noise(float fx, float fy, int octaves, float persistence) {
         float amplitude = 1;
         float max = 0;
         float result = 0;
 
-        while (octaves-- > 0)
-        {
+        while (octaves-- > 0) {
             max += amplitude;
             result += noise(fx, fy) * amplitude;
             amplitude *= persistence;
             fx *= 2;
             fy *= 2;
         }
-        return result/max;
+        return result / max;
     }
 
-    public BufferedImage getMap() {
-        BufferedImage map = new BufferedImage(LevelGenerator.resolution, LevelGenerator.resolution, BufferedImage.TYPE_INT_RGB);
-        int[] pixels = ((DataBufferInt) map.getRaster().getDataBuffer()).getData();
-        for (int xx = 0; xx < LevelGenerator.resolution; xx++) {
-            for (int yy = 0; yy < LevelGenerator.resolution; yy++) {
 
-                float newVal = noise(xx/100f, yy/100f,8,0.5f);
-                pixels[xx * LevelGenerator.resolution + yy] = (int)(newVal * 255 + 128) & 255;
+    public int[][] getMap() {
+        int[][] res = new int[resolution][resolution];
+        for (int xx = 0; xx < resolution; xx++) {
+            for (int yy = 0; yy < resolution; yy++) {
+
+                float newVal = noise(xx / 100f, yy / 100f, 8, 0.5f);
+                res[xx][yy] = (int) (newVal * 255 + 128) & 255;
             }
         }
-        System.out.println(Arrays.toString(pixels));
+        return res;
+    }
+
+    public BufferedImage getImage() {
+        BufferedImage map = new BufferedImage(resolution, resolution, BufferedImage.TYPE_INT_RGB);
+        int[] pixels = ((DataBufferInt) map.getRaster().getDataBuffer()).getData();
+        for (int xx = 0; xx < resolution; xx++) {
+            for (int yy = 0; yy < resolution; yy++) {
+
+                float newVal = noise(xx / 100f, yy / 100f, 8, 0.5f);
+                pixels[xx * resolution + yy] = (int) (newVal * 255 + 128) & 255;
+            }
+        }
         return map;
     }
 }
