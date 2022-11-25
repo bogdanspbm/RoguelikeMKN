130f2ba681c7fd60602fd321e1fae1a90589cfd1
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-11-25 13:47:00 +0300

    Inventory Tests

--- src/test/java/inventoryTests/InventoryConsoleTests.java
+++ src/test/java/inventoryTests/InventoryConsoleTests.java
@@ -15,4 +15,28 @@ public class InventoryConsoleTests {
         inventory.addItem(new Item(1, 5));
         Assert.assertEquals(1, inventory.getItemCount());
     }
+
+    @Test
+    public void itemSwapTest() {
+        inventory.addItem(new Item(1, 7));
+
+        Assert.assertEquals(5, inventory.getItemByIndex(0).getQuantity());
+        Assert.assertEquals(2, inventory.getItemByIndex(1).getQuantity());
+
+        inventory.moveItemToSlot(0, 1);
+
+        Assert.assertEquals(2, inventory.getItemByIndex(0).getQuantity());
+        Assert.assertEquals(5, inventory.getItemByIndex(1).getQuantity());
+    }
+
+    @Test
+    public void itemMove() {
+        inventory.addItem(new Item(2, 1));
+
+        Assert.assertEquals(2, inventory.getItemByIndex(0).getId());
+
+        inventory.moveItemToSlot(0, 1);
+
+        Assert.assertEquals(2, inventory.getItemByIndex(1).getId());
+    }
 }
