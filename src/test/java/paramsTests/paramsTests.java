0289b7adc93fa1855d69e00de4e208dba7847b28
Bogdan Madzhuga <43291305+bogdanspbm@users.noreply.github.com>
2022-12-26 19:59:22 +0300

    level App

---
+++ src/test/java/paramsTests/paramsTests.java
@@ -0,0 +1,25 @@
+package paramsTests;
+
+
+import enemies.Enemy;
+import enums.EBotType;
+import enums.EPawnStatus;
+import objects.pawn.Pawn;
+import org.junit.Assert;
+import org.junit.Test;
+import params.ParamsComponent;
+import params.interfaces.Params;
+import structures.Vector3D;
+
+public class paramsTests {
+
+    @Test
+    public void checkLevelApp() {
+        ParamsComponent paramsComponent = new ParamsComponent();
+        Assert.assertEquals(paramsComponent.getLevel(), 1);
+        paramsComponent.addExperience(10000);
+        Assert.assertEquals(paramsComponent.getLevel() > 1, true);
+    }
+    
+
+}
